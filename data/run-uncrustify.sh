3a9f6f38a2ea5a8577818359ceb3ccd597eba85d
jaimegh-es <github@inled.es>
2026-08-21 16:26:55 +0200

    feat: Initial commit of Finder for Pulsar OS (forked from GNOME Files 48.7)

---
+++ data/run-uncrustify.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+DATA=$(dirname "$BASH_SOURCE")
+UNCRUSTIFY=$(command -v uncrustify)
+
+if [ -z "$UNCRUSTIFY" ];
+then
+    echo "Uncrustify is not installed on your system."
+    exit 1
+fi
+
+if [ ! -x "$DATA/lineup-parameters" ];
+then
+    echo "Script lineup-parameters does not exists."
+    exit 1
+fi
+
+for DIR in "$DATA/../"{src,test,libnautilus-extension,eel,extensions}
+do
+    for FILE in $(find "$DIR" -name "*.c" -not -path "*/gtk/*")
+    do
+        # Aligning prototypes is not working yet, so avoid headers
+        "$UNCRUSTIFY" -c "$DATA/uncrustify.cfg" --no-backup "$FILE" &&
+        "$DATA/lineup-parameters" "$FILE" > "$FILE.temp" && mv "$FILE.temp" "$FILE" &
+   done
+done
+
+wait
